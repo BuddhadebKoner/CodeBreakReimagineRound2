d318e357e6a5c9892706b01502073c49713abf9d
BuddhadebKoner <jeetkoner36@gmail.com>
2024-07-16 18:04:12 +0530

    vite chnages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CodeBreakReimagineRound2",
+  // base: "/CodeBreakReimagineRound2",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
