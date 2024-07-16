d5397df8a05eadaa230f85ab0d19fe12b731c325
BuddhadebKoner <jeetkoner36@gmail.com>
2024-07-16 09:21:14 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Hovercircle';
+import Globe from './components/magicui/globe';
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <Globe/>
+    </>
+  );
+}
+
+export default App;
