f1e470c012538e6024ff60e7726be5077b4cd5d9
BuddhadebKoner <jeetkoner36@gmail.com>
2024-07-16 17:39:19 +0530

    navbar structure

--- src/components/Herosections.jsx
+++ src/components/Herosections.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import Globe from "./magicui/globe";
 import Navbar from "./Navbar";
 import "../styles/hero.css";
+import BoxReveal from "./magicui/box-reveal";
 
 export default function Herosections() {
-   return (
-      <>
-         <div className="hero_container">
-            <Navbar />
-            <Globe />
-            <h1>hello</h1>
-         </div>
-      </>
-   );
+
+   const speedOfHeroText = 1
+
+
+  return (
+    <div className="hero_nav_container">
+      <Navbar />
+      <div className="hero_content">
+        <Globe className="custom-globe" />
+        <BoxReveal boxColor={"#ffffff"} duration={speedOfHeroText}>
+          <h1 className="hero_title">INDIA POST</h1>
+        </BoxReveal>
+        <BoxReveal boxColor={"#ffffff"} duration={speedOfHeroText}>
+          <p>
+            Urgent Hai ?<br /> speed post kardo na !
+          </p>
+        </BoxReveal>
+        <BoxReveal boxColor={"#ffffff"} duration={speedOfHeroText}>
+          <button className="hero_btn" >Post Now</button>
+        </BoxReveal>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,82 @@
-import React, { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import '../styles/navbar.css';
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "../styles/navbar.css";
 
 export default function Navbar() {
-  const container = useRef();
+   const container = useRef();
 
-  useGSAP(() => {
-    const t1 = gsap.timeline({ paused: true });
+   useGSAP(
+      () => {
+         const t1 = gsap.timeline({ paused: true });
 
-    t1.to(".one", 0.5, {
-      y: 6,
-      rotation: 45,
-      ease: "expo.inOut",
-    });
-    t1.to(".two", 0.5, {
-      y: -6,
-      rotation: -45,
-      ease: "expo.inOut",
-      delay: -0.5,
-    });
+         t1.to(".one", 0.5, {
+            y: 6,
+            rotation: 45,
+            ease: "expo.inOut",
+         });
+         t1.to(".two", 0.5, {
+            y: -6,
+            rotation: -45,
+            ease: "expo.inOut",
+            delay: -0.5,
+         });
 
-    t1.to(".menu", 1.5, {
-      top: "0%",
-      ease: "expo.inOut",
-      delay: -0.5,
-    });
+         t1.to(".menu", 1.5, {
+            top: "0%",
+            ease: "expo.inOut",
+            delay: -0.5,
+         });
 
-    t1.staggerFrom(
-      ".menu ul li",
-      0.75,
-      { x: -30, opacity: 0, ease: "expo.out" },
-      0.1
-    );
+         t1.staggerFrom(
+            ".menu ul li",
+            0.75,
+            { x: -30, opacity: 0, ease: "expo.out" },
+            0.1
+         );
 
-    t1.reverse();
+         t1.reverse();
 
-    const handleClick = () => {
-      t1.reversed(!t1.reversed());
-    };
+         const handleClick = () => {
+            t1.reversed(!t1.reversed());
+         };
 
-    const buttons = container.current.querySelectorAll('.toggle-btn, a');
-    buttons.forEach(btn => btn.addEventListener('click', handleClick));
+         const buttons = container.current.querySelectorAll(".toggle-btn, a");
+         buttons.forEach((btn) => btn.addEventListener("click", handleClick));
 
-    return () => {
-      buttons.forEach(btn => btn.removeEventListener('click', handleClick));
-    };
-  }, { scope: container });
+         return () => {
+            buttons.forEach((btn) =>
+               btn.removeEventListener("click", handleClick)
+            );
+         };
+      },
+      { scope: container }
+   );
 
-  return (
-    <div ref={container}>
-      <div className="toggle-btn">
-        <span className="one"></span>
-        <span className="two"></span>
+   return (
+      <div ref={container}>
+         <div className="toggle-btn">
+            <span className="one"></span>
+            <span className="two"></span>
+         </div>
+         <div className="menu">
+            <div className="data">
+               <ul>
+                  <li>
+                     <a href="#">home</a>
+                  </li>
+                  <li>
+                     <a href="#">about</a>
+                  </li>
+                  <li>
+                     <a href="#">services</a>
+                  </li>
+                  <li>
+                     <a href="#">contact</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
       </div>
-      <div className="menu">
-        <div className="data">
-          <ul>
-            <li><a href="#">home</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">services</a></li>
-            <li><a href="#">contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+   );
 }
